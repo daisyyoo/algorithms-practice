f7b87413acf8699809cecd03341150d48d3ce662
Daisy Yoo <daisyhyoo@gmail.com>
2022-12-20 23:09:32 +0000

    Working on code signal 4

---
+++ __sample__/code-signal-4.js
@@ -0,0 +1,8 @@
+// function solution(inputArray) {
+//   const first = inputArray[0]
+//   const second = inputArray[1]
+//   let total = first * second;
+//   for (let i = 0; i < inputArray.length; i++) {
+//     console.log('hello');
+//   }
+// }
